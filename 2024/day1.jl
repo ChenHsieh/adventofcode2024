86156d0545bbe1ab63297a4cac0bd8ed73acc1c4
chen <chen.hsieh@uga.edu>
2024-12-01 11:11:59 -0500

    init and day1

---
+++ 2024/day1.jl
@@ -0,0 +1,32 @@
+# using Pkg
+# Pkg.add(["CSV", "DataFrames"])
+
+
+using CSV
+using DataFrames
+
+# Define the file path
+ts_file = "data/day1_input.txt"  # Replace this with your actual file path
+
+# Read the TSV file into a DataFrame
+df = CSV.read(ts_file, DataFrame; delim=' ', ignorerepeated=true, header=0)
+
+# Sort each column from small to large
+df_sorted = DataFrame([c => sort(df[!, c]) for c in names(df)])
+
+
+# Calculate the sum of the absolute difference between column 1 and column 2 of df_sorted using `eachindex`
+abs_diff_sum_sorted = sum(abs(df_sorted[i, 1] - df_sorted[i, 2]) for i in eachindex(df_sorted[:, 1]))
+
+# Print the result
+println("Sum of absolute differences (sorted DataFrame): ", abs_diff_sum_sorted)
+
+
+# Calculate the total similarity score
+left_list = df_sorted[:, 1]
+right_list = df_sorted[:, 2]
+
+similarity_score = sum(left_value * count(==(left_value), right_list) for left_value in left_list)
+
+# Print the similarity score
+println("Total similarity score: ", similarity_score)
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # adventofcode2024
+
+I want to take this opportunity to learn a new programming language. I have chosen to learn Julia.
+
+All input files were downloaded by directly saving the web page
+
